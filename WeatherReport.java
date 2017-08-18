a7a4efafd44a0a78ea1f9f760ce1a17132a8ed27
LSALBIN <31044188+LSALBIN@users.noreply.github.com>
2017-08-18 12:09:54 -0500

    Add files via upload

--- WeatherReport.java
+++ WeatherReport.java
@@ -1,11 +1,34 @@
 import java.util.Scanner;
 
-public class WeatherReport
+public class WeatherReport 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
+		Scanner cin = new Scanner(System.in);
 		
+		System.out.println("Hello! Please enter your temperature in Farenheit!");
+		
+		int temp = cin.nextInt();
+		
+		int ans = temp;
+		
+		{
+			ans = (((temp - 32)*5)/9);
+		}
+		
+		System.out.println("You're temperature is" +" " + ans +" " + "centigrade");
+		
+		if(temp<=32)
+		{
+			System.out.println("It's cold outside!");
+		}
+		else if(temp>=90)
+		{
+			System.out.println("It's hot outside!");
+		}
+		cin.close();
+			
 	}
 
 }
